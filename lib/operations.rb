eeddbc9d6e76646ce85ace1bd88cd090ffdf3569
Nick Dunlap <nwdunlap17@gmail.com>
2019-07-16 10:40:37 -0700

    done

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+    if speed > 60 || speed < 40
+        return true
+    end
+    return false
 end
 
 
 
 def not_safe?(speed)
-	
+	return (speed>60||speed<40) ? true : false
 end
 	
 
